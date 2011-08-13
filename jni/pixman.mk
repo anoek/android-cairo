ec5d6e97e94218ca9ac09dd0393d2da1551cc958
Akita Noek <akitanoek@gmail.com>
2011-08-13 17:48:49 -0400

    Synced pixman to the latest stable tag, 0.22.2

--- jni/pixman.mk
+++ jni/pixman.mk
@@ -1,34 +1,35 @@
 LOCAL_PATH := $(call my-dir)
 
 LIBPIXMAN_SRC= \
-	       pixman/pixman/pixman-access.c           \
-	       pixman/pixman/pixman-access-accessors.c \
-	       pixman/pixman/pixman-cpu.c              \
-	       pixman/pixman/pixman-gradient-walker.c  \
-	       pixman/pixman/pixman-region16.c         \
-	       pixman/pixman/pixman-region32.c         \
-	       pixman/pixman/pixman-image.c            \
-	       pixman/pixman/pixman-implementation.c   \
-	       pixman/pixman/pixman-general.c          \
-	       pixman/pixman/pixman.c                  \
-	       pixman/pixman/pixman-fast-path.c        \
-	       pixman/pixman/pixman-solid-fill.c       \
-	       pixman/pixman/pixman-conical-gradient.c \
-	       pixman/pixman/pixman-linear-gradient.c  \
-	       pixman/pixman/pixman-radial-gradient.c  \
-	       pixman/pixman/pixman-bits-image.c       \
-	       pixman/pixman/pixman-utils.c            \
-	       pixman/pixman/pixman-edge.c             \
-	       pixman/pixman/pixman-edge-accessors.c   \
-	       pixman/pixman/pixman-trap.c             \
-	       pixman/pixman/pixman-timer.c            \
-	       pixman/pixman/pixman-matrix.c           \
-	       pixman/pixman/pixman-arm-simd.c         \
-	       pixman/pixman/pixman-arm-simd-asm.S     \
-	       pixman/pixman/pixman-arm-neon.c         \
-	       pixman/pixman/pixman-arm-neon-asm.S     \
-	       pixman-extra/pixman-combine32.c         \
-	       pixman-extra/pixman-combine64.c         \
+	       pixman/pixman/pixman-access.c                \
+	       pixman/pixman/pixman-access-accessors.c      \
+	       pixman/pixman/pixman-cpu.c                   \
+	       pixman/pixman/pixman-gradient-walker.c       \
+	       pixman/pixman/pixman-region16.c              \
+	       pixman/pixman/pixman-region32.c              \
+	       pixman/pixman/pixman-image.c                 \
+	       pixman/pixman/pixman-implementation.c        \
+	       pixman/pixman/pixman-general.c               \
+	       pixman/pixman/pixman.c                       \
+	       pixman/pixman/pixman-fast-path.c             \
+	       pixman/pixman/pixman-solid-fill.c            \
+	       pixman/pixman/pixman-conical-gradient.c      \
+	       pixman/pixman/pixman-linear-gradient.c       \
+	       pixman/pixman/pixman-radial-gradient.c       \
+	       pixman/pixman/pixman-bits-image.c            \
+	       pixman/pixman/pixman-utils.c                 \
+	       pixman/pixman/pixman-edge.c                  \
+	       pixman/pixman/pixman-edge-accessors.c        \
+	       pixman/pixman/pixman-trap.c                  \
+	       pixman/pixman/pixman-timer.c                 \
+	       pixman/pixman/pixman-matrix.c                \
+	       pixman/pixman/pixman-arm-simd.c              \
+	       pixman/pixman/pixman-arm-simd-asm.S          \
+	       pixman/pixman/pixman-arm-neon.c              \
+	       pixman/pixman/pixman-arm-neon-asm.S          \
+	       pixman/pixman/pixman-arm-neon-asm-bilinear.S \
+	       pixman-extra/pixman-combine32.c              \
+	       pixman-extra/pixman-combine64.c              \
 
 
 
