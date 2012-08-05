190d975df0ed574c52be7ed121017471412d4990
Andrea Brancaleoni <virus@gmx.us>
2012-08-06 01:35:25 +0200

    Pixman version 0.27.2

--- jni/pixman.mk
+++ jni/pixman.mk
@@ -1,39 +1,48 @@
 LOCAL_PATH := $(call my-dir)
 
 LIBPIXMAN_SRC= \
-	       pixman/pixman/pixman-access.c                \
-	       pixman/pixman/pixman-access-accessors.c      \
-	       pixman/pixman/pixman-cpu.c                   \
-	       pixman/pixman/pixman-gradient-walker.c       \
-	       pixman/pixman/pixman-region16.c              \
-	       pixman/pixman/pixman-region32.c              \
-	       pixman/pixman/pixman-image.c                 \
-	       pixman/pixman/pixman-implementation.c        \
-	       pixman/pixman/pixman-general.c               \
-	       pixman/pixman/pixman.c                       \
-	       pixman/pixman/pixman-fast-path.c             \
-	       pixman/pixman/pixman-solid-fill.c            \
-	       pixman/pixman/pixman-conical-gradient.c      \
-	       pixman/pixman/pixman-linear-gradient.c       \
-	       pixman/pixman/pixman-radial-gradient.c       \
-	       pixman/pixman/pixman-bits-image.c            \
-	       pixman/pixman/pixman-utils.c                 \
-	       pixman/pixman/pixman-edge.c                  \
-	       pixman/pixman/pixman-edge-accessors.c        \
-	       pixman/pixman/pixman-trap.c                  \
-	       pixman/pixman/pixman-timer.c                 \
-	       pixman/pixman/pixman-matrix.c                \
-	       pixman/pixman/pixman-arm-simd.c              \
-	       pixman/pixman/pixman-arm-simd-asm.S          \
-	       pixman/pixman/pixman-arm-neon.c              \
-	       pixman/pixman/pixman-arm-neon-asm.S          \
-	       pixman/pixman/pixman-arm-neon-asm-bilinear.S \
-	       pixman-extra/pixman-combine32.c              \
-	       pixman-extra/pixman-combine64.c              \
+pixman/pixman/pixman-access-accessors.c \
+pixman/pixman/pixman-access.c \
+pixman/pixman/pixman-arm.c \
+pixman/pixman/pixman-arm-neon.c \
+pixman/pixman/pixman-arm-simd.c \
+pixman/pixman/pixman-bits-image.c \
+pixman/pixman/pixman.c \
+pixman/pixman/pixman-conical-gradient.c \
+pixman/pixman/pixman-edge-accessors.c \
+pixman/pixman/pixman-edge.c \
+pixman/pixman/pixman-fast-path.c \
+pixman/pixman/pixman-general.c \
+pixman/pixman/pixman-glyph.c \
+pixman/pixman/pixman-gradient-walker.c \
+pixman/pixman/pixman-image.c \
+pixman/pixman/pixman-implementation.c \
+pixman/pixman/pixman-linear-gradient.c \
+pixman/pixman/pixman-matrix.c \
+pixman/pixman/pixman-mips.c \
+pixman/pixman/pixman-mips-dspr2.c \
+pixman/pixman/pixman-mmx.c \
+pixman/pixman/pixman-noop.c \
+pixman/pixman/pixman-ppc.c \
+pixman/pixman/pixman-radial-gradient.c \
+pixman/pixman/pixman-region16.c \
+pixman/pixman/pixman-region32.c \
+pixman/pixman/pixman-solid-fill.c \
+pixman/pixman/pixman-timer.c \
+pixman/pixman/pixman-trap.c \
+pixman/pixman/pixman-utils.c \
+pixman/pixman/pixman-x86.c \
+\
+pixman/pixman/pixman-arm-neon-asm-bilinear.S \
+pixman/pixman/pixman-arm-neon-asm.S \
+pixman/pixman/pixman-arm-simd-asm.S \
+\
+pixman-extra/pixman-combine32.c \
+pixman-extra/pixman-combine64.c \
+pixman-extra/pixman-srgb.c \
+pixman-extra/cpu-features.c \
 
-
-
-LIBPIXMAN_CFLAGS:=-D_USE_MATH_DEFINES -DPIXMAN_NO_TLS -DPACKAGE="android-canvas" -DUSE_ARM_NEON -DUSE_ARM_SIMD
+LIBPIXMAN_CFLAGS:=-D_USE_MATH_DEFINES -DPIXMAN_NO_TLS -DPACKAGE="android-canvas" -DUSE_ARM_NEON -DUSE_ARM_SIMD -DSIZE_MAX=0xfffffff
 
 include $(CLEAR_VARS)
 
